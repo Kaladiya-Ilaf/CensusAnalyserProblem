dc366afb75271848fe517691b6dc02c47e83ace4
Kaladiya-Ilaf <ilafkaladiya03@gmail.com>
2020-08-19 23:21:54 +0530

    Refactored : To handle wrong csv and used stream to return count.

--- CensusAnalyser/src/main/java/censusanalyser/CensusAnalyser.java
+++ CensusAnalyser/src/main/java/censusanalyser/CensusAnalyser.java
@@ -8,6 +8,7 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
     public static int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
@@ -17,18 +18,18 @@ public class CensusAnalyser {
             csvToBeanBuilder.withType(IndiaCensusCSV.class);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
             CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
-
             Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
-            int numberOfEntries = 0;
-            while (censusCSVIterator.hasNext()){
-                numberOfEntries += 1;
-                IndiaCensusCSV censusData = censusCSVIterator.next();
-            }
-            return numberOfEntries;
+            Iterable<IndiaCensusCSV> csvIterable = () -> censusCSVIterator;
+            return (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
         } catch (IOException e) {
-            throw new CensusAnalyserException(e.getMessage(),CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+            throw new CensusAnalyserException(e.getMessage(),
+                                              CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        }catch (IllegalStateException e){
+            throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+
         } catch (RuntimeException e){
-            throw new CensusAnalyserException(e.getMessage(),CensusAnalyserException.ExceptionType.INCORRECT_DATA_PROBLEM);
+            throw new CensusAnalyserException(e.getMessage(),
+                                              CensusAnalyserException.ExceptionType.INCORRECT_DATA_PROBLEM);
         }
 
 
