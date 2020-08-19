60ac611e808abaf871266883d6377d99be4414da
Kaladiya-Ilaf <ilafkaladiya03@gmail.com>
2020-08-19 23:23:19 +0530

    Modified : Indentation and spaces as requires

--- CensusAnalyser/src/main/java/censusanalyser/CensusAnalyser.java
+++ CensusAnalyser/src/main/java/censusanalyser/CensusAnalyser.java
@@ -2,7 +2,6 @@ package censusanalyser;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -18,18 +17,17 @@ public class CensusAnalyser {
             csvToBeanBuilder.withType(IndiaCensusCSV.class);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
             CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
+
             Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
             Iterable<IndiaCensusCSV> csvIterable = () -> censusCSVIterator;
             return (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
         } catch (IOException e) {
-            throw new CensusAnalyserException(e.getMessage(),
-                                              CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+            throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         }catch (IllegalStateException e){
             throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
 
         } catch (RuntimeException e){
-            throw new CensusAnalyserException(e.getMessage(),
-                                              CensusAnalyserException.ExceptionType.INCORRECT_DATA_PROBLEM);
+            throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.INCORRECT_DATA_PROBLEM);
         }
 
 
